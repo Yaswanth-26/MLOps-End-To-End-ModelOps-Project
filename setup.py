c3400c23cf92db2232e0b54590f93ba883d610a8
Yaswanth-26 <yaswanthns26@gmail.com>
2026-03-06 00:52:14 +0530

    MLOps-End-To-End-ModelOps-Project

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "MLOps-End-To-End-ModelOps-Project"
+AUTHOR_USER_NAME = "Yaswanth Reddy"
+SRC_REPO = "mlopsproject"
+AUTHOR_EMAIL = "yaswanthns26@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Mlpos foundation project",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/Yaswanth-26/MLOps-End-To-End-ModelOps-Project.git",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
